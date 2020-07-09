09afb062990e8a0d097ee110c533b35b2e6f8ea6
Teldolar <arturgareev2000artur@gmail.com>
2020-07-09 15:20:45 +0300

    new

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,12 +9,14 @@ namespace WindowsFormsApp1
         MainMenu MnMen1;
         MenuItem pnkt1;
         MenuItem pnkt2;
+        MenuItem info;
         public Form1()
         {
             InitializeComponent();
             pnkt1 = new MenuItem("Пуск", new EventHandler(timer1_Tick), Shortcut.Alt1);
             pnkt2 = new MenuItem("Стоп", new EventHandler(stop), Shortcut.Alt2);
-            MnMen1 = new MainMenu(new MenuItem[] { pnkt1, pnkt2 });
+            info = new MenuItem("Информация о разработчике", new EventHandler(InfoORazrabotchik), Shortcut.Alt3);
+            MnMen1 = new MainMenu(new MenuItem[] { pnkt1, pnkt2, info });
             this.Menu = MnMen1;
             cir = new circle[3];
             cir[0] = new circle(new Point(0, 200),"Right",false,Brushes.Red);
@@ -27,6 +29,10 @@ namespace WindowsFormsApp1
         private void stop(object sender, EventArgs e)
         {
             timer1.Enabled = false;
+        }
+        private void InfoORazrabotchik(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выполнил: студент группы 4208\nГареев А.И.\nВариант работы: 3", "Информация о разработчике");
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
